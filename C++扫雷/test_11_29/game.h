5e20b39a299b8bd10c09c348025ff7a71f486dc0
Flaviussun20 <742724666@qq.com>
2020-12-14 12:56:29 +0800

    C++扫雷

---
+++ C++扫雷/test_11_29/game.h
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include<stdlib.h>
+#include<time.h>
+#define ROW 9
+#define COL 9
+#define ROWS ROW+2
+#define COLS COL+2
+#define EASY_COUNT 80
+
+//≥ı ºªØ∆Â≈Ã
+void InitBoard(char arr[ROWS][COLS], int rows, int cols,char set);
+//¥Ú”°∆Â≈Ã
+void DisplayBoard(char arr[ROWS][COLS],int row,int col);
+//…Ë÷√¿◊
+void SetMine(char mine[ROWS][COLS], int row, int col);
+//≈≈≤È¿◊
+void FindMine(char mine[ROWS][COLS], char show[ROWS][COLS], int row, int col);
